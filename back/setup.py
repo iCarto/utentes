4c68770ac62fbb2ddfe0b06610f6b317abeb0952
Francisco Puga <fpuga@icarto.es>
2021-08-25 16:13:26 +0200

    New version: 210825

--- back/setup.py
+++ back/setup.py
@@ -31,7 +31,7 @@ excludes = ["utentes.tests"]
 
 setup(
     name="utentes",
-    version="210607",
+    version="210825",
     description="utentes",
     author="iCarto",
     author_email="info@icarto.es",
