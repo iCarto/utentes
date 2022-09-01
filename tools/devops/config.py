7402b70f800f7140819fb2591a63498a34860b48
Francisco Puga <fpuga@icarto.es>
2022-09-01 14:30:23 +0200

    Minor adjustements after repo reorganization

---
+++ tools/devops/config.py
@@ -0,0 +1,3 @@
+remote_backup_folder = "/var/www/sirha_{hostname}/backups/"
+local_backup_folder = "../../../bck-sirha/sirha_{hostname}/backups/"
+local_entregas_folder = "../../../bck-sirha/entregas/"
