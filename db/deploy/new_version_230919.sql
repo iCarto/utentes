06707c4c1bc374bfc273cb36e1a4aee36de9a9a8
Francisco Puga <fpuga@icarto.es>
2023-09-19 22:21:14 +0200

    Fixes not needed update table version

--- db/deploy/new_version_230919.sql
+++ db/deploy/new_version_230919.sql
@@ -1,11 +1,11 @@
 -- Deploy utentes:new_version_230919 to pg
 
 BEGIN;
-
+/*
 DELETE FROM utentes.version;
 DELETE FROM inventario.version;
 
 INSERT INTO utentes.version (version) VALUES ('230919');
 INSERT INTO inventario.version (version) VALUES ('230919');
-
+*/
 COMMIT;
--- db/revert/new_version_230919.sql
+++ db/revert/new_version_230919.sql
@@ -1,11 +1,11 @@
 -- Revert utentes:new_version_230919 from pg
 
 BEGIN;
-
+/*
 DELETE FROM utentes.version;
 DELETE FROM inventario.version;
 
 INSERT INTO utentes.version (version) VALUES ('220311');
 INSERT INTO inventario.version (version) VALUES ('220311');
-
+*/
 COMMIT;
