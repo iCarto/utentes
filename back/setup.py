a398b9821b5f31031842d9204af855aa8738bde6
Francisco Puga <fpuga@icarto.es>
2023-09-19 22:08:47 +0200

    New version: 230919

--- back/setup.py
+++ back/setup.py
@@ -34,7 +34,7 @@ excludes = ["utentes.tests"]
 
 setup(
     name="utentes",
-    version="220921",
+    version="230919",
     description="utentes",
     author="iCarto",
     author_email="info@icarto.es",
